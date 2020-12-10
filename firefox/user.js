3898451803ef41f6cb779829c97c9c043bc57583
sasaplus1 <sasaplus1@gmail.com>
2020-12-10 11:02:38 +0900

    update user.js

--- firefox/user.js
+++ firefox/user.js
@@ -1,11 +1,26 @@
+// Ctrl+Tabをタブの並び順にする
+user_pref('browser.ctrlTab.recentlyUsedOrder', false);
+
 // ダウンロードファイルを最近使ったファイルに追加しない
 user_pref('browser.download.manager.addToRecentDocs', false);
 
-// 検索結果を新しいタブで開かない（デフォルト）
-user_pref('browser.search.openintab', false);
+// ファイルダウンロード時にポップアップを表示する
+user_pref('browser.download.panel.shown', true);
+
+// Activity Streamの各種feedを非表示にする
+user_pref('browser.newtabpage.activity-stream.feeds.section.highlights', false);
+user_pref('browser.newtabpage.activity-stream.feeds.snippets', false);
+user_pref('browser.newtabpage.activity-stream.feeds.topsites', false);
 
 // 最後のタブを閉じれないようにする
 user_pref('browser.tabs.closeWindowWithLastTab', false);
+
+// ブラウザコンソールを有効にする
+// https://developer.mozilla.org/ja/docs/Tools/Browser_Console
+user_pref('devtools.chrome.enabled', true);
+
+// おすすめの拡張機能を非表示にする
+user_pref('extensions.htmlaboutaddons.recommendations.enabled', false)
 
 // mailto:をクリックしてもメーラーを起動しない
 user_pref('network.protocol-handler.external.mailto', false);
