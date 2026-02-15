42dbe7bfd91c760ea4ede40ec9bcf924f6b5e0ab
sasaplus1 <sasaplus1@gmail.com>
2026-02-16 01:02:17 +0900

    add home-manager settings

---
+++ home-manager/flake.nix
@@ -0,0 +1,33 @@
+# NOTE: need --impure
+# $ home-manager switch --flake . --impure
+{
+  description = "Home Manager configuration";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, home-manager, ... }:
+    let
+      system = builtins.currentSystem;
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations.${builtins.getEnv "USER"} = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
