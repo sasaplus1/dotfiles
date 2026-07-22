c428d6d9cbd1d1a2c7bc8b58ee4c097244c703bf
sasaplus1 <sasaplus1@gmail.com>
2026-07-22 16:27:08 +0900

    support other remotes

--- git/hooks/pre-push-gitleaks.sh
+++ git/hooks/pre-push-gitleaks.sh
@@ -12,6 +12,9 @@ cyan=$'\033[01;36m'
 reset=$'\033[0m'
 yellow=$'\033[33m'
 
+# git は push 先リモート名を $1、URL を $2 で渡す
+remote_name="$1"
+
 while read -r _local_ref local_sha _remote_ref remote_sha
 do
   # ブランチ削除の場合はスキップ
@@ -26,15 +29,21 @@ do
     # 理想: remote_shaがローカルに存在する
     range="$remote_sha..$local_sha"
   else
-    # フォールバック: originに未到達の全コミットをスキャン
+    # フォールバック: push先リモートに未到達の全コミットをスキャン
     # 新規ブランチ、remote_sha欠落、force push等で使用
     if [ "$remote_sha" = "$z40" ]
     then
-      printf -- '%sInfo: new branch, scanning commits not yet in origin%s\n' "$cyan" "$reset" >&2
+      printf -- '%sInfo: new branch, scanning commits not yet in %s%s\n' "$cyan" "$remote_name" "$reset" >&2
     else
-      printf -- '%sWarning: remote SHA not found locally, scanning commits not yet in origin%s\n' "$yellow" "$reset" >&2
+      printf -- '%sWarning: remote SHA not found locally, scanning commits not yet in %s%s\n' "$yellow" "$remote_name" "$reset" >&2
     fi
-    range="$local_sha --not --remotes=origin"
+    # push先が名前付きリモートならそのリモートを、URL直push等なら全リモートを除外対象にする
+    if git remote | grep -Fqx -- "$remote_name"
+    then
+      range="$local_sha --not --remotes=$remote_name"
+    else
+      range="$local_sha --not --remotes"
+    fi
   fi
 
   gitleaks git --log-opts="$range" --no-banner --verbose || exit 1
